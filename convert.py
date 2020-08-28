1c20e7123ab4a541c7bf1eb8ab825e0d527aec68
zunda-pixel <zunda.pixel@gmail.com>
2020-08-28 21:53:12 +0900

    一枚の画像を複製することで一定間隔表示できるように

--- convert.py
+++ convert.py
@@ -2,11 +2,14 @@ import glob
 import cv2
 
 img_array = []
+second = 1
+
 for filename in sorted(glob.glob("images/*.jpg")):
     img = cv2.imread(filename)
     height, width, layers = img.shape
     size = (width, height)
-    img_array.append(img)
+    for tmp in range(second):
+        img_array.append(img)
 
 name = 'project.mp4'
 out = cv2.VideoWriter(name, cv2.VideoWriter_fourcc(*'MP4V'), 5.0, size)
